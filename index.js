fa107d9db91c9962571e011c15f743836057b395
Blair Learn <blairl-nih@users.noreply.github.com>
2020-12-02 18:05:30 -0500

    (#1) Introduce connection pooling. - Limit the number of simultaneous async http calls. - Use agentkeepalive to manage connections and sockets.

--- index.js
+++ index.js
@@ -1,34 +1,68 @@
 'use strict'
 
-const axios = require('axios');
-
 const fs = require('fs');
+const asyncPool = require('tiny-async-pool');
+const axios = require('axios');
+const Agent = require('agentkeepalive');
+
+const keepAliveConfig = {
+    maxSockets: 40,
+    timeout: 30000
+};
+
+// Axios uses separate agents for HTTP vs HTTPS.
+const httpKeepAliveAgent = new Agent(keepAliveConfig);
+const httpsKeepAliveAgent = new Agent.HttpsAgent(keepAliveConfig);
+
+const requestConfig = {
+    maxRedirects: 0,
+    validateStatus: function(status) {
+        return true;
+    }
+};
+
+const httpClient = axios.create({
+    httpAgent: httpKeepAliveAgent,
+    httpsAgent: httpsKeepAliveAgent
+});
+
+async function test(url) {
+
+    try {
+        return await httpClient.get(url, requestConfig);
+    } catch (error) {
+        return `Error '${error.message}' connecting to '${url}'.`;
+    }
+}
 
 async function main() {
 
     const temp = fs.readFileSync('urls.json');
     const urlList = JSON.parse(temp);
 
-    const requestConfig = {
-        maxRedirects: 0,
-        validateStatus: function(status) {
-            return true;
-        }
-    };
+    const responses = await asyncPool(5, urlList, test);
 
-    const promises = urlList.map((url) => axios.get(url, requestConfig));
-    const responses = await Promise.all(promises);
-
-    let allOK = true;
+    let errorList = new Array();
     responses.forEach((response) => {
-        if( response.status != 200 ) {
-            allOK = false;
-            console.log(`Unexpected status '${response.status}' for url '${response.config.url}'`);
+
+        // If we got back an object, it's an HTTP response so check for an unexpected result and report it.
+        // If it's a string, something went badly wrong so we display it.
+        // Otherwise, it's a valid HTTP response, and we do nothing further.
+        if( typeof(response) == 'object' && response.status != 200 ) {
+            errorList.push(`Unexpected status '${response.status}' for url '${response.config.url}'`);
+        }
+        else if( typeof(response) == 'string' ) {
+            errorList.push(response);
         }
     });
 
-    if( allOK )
-        console.log('All urls OK.')
+    if( errorList.length == 0 ) {
+        console.error('All urls OK.')
+    }
+    else {
+        console.error("Errors encountered:\n");
+        errorList.forEach(message => console.error(message));
+    }
 }
 
 main();
